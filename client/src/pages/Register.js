c08194e113ad0a1c920a2b2614a12d69865433b2
Regina Zaripova <rnloftz@gmail.com>
2020-11-30 21:50:28 +1000

    get register user data | set react state with register user data

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -15,6 +15,16 @@ export default function Register() {
     const {name, lName, email, password, passwordTwo} = formData;
 
    const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        if(password !== passwordTwo){
+            console.log("Passwords do not match")
+        } else {
+            console.log(formData)
+        }
+    }
     
 
     return (
@@ -28,26 +38,27 @@ export default function Register() {
                 <div className="card flex-center-column">
                     <div  className="flex-center-column form-container-sign">
                         <h3>Sign Up</h3>
-                        <form>
+                        <form onSubmit={e => onSubmit(e)}>
                             <div className="form-item">
                                 <label htmlFor="name" className="block">First Name</label>
-                                <input className="form-control" type="text" id="name" name="name" value={formData.name} onChange={e => onChange(e)} required/>
+                                <input className="form-control" type="text" id="name" name="name" value={name} onChange={e => onChange(e)} required/>
                             </div>
                             <div className="form-item">
                                 <label htmlFor="lName" className="block">Last Name</label>
-                                <input className="form-control" type="text" id="lName" name="lName" />
+                                <input className="form-control" type="text" id="lName" name="lName" value={lName} onChange={e => onChange(e)} required />
                             </div>
                             <div className="form-item">
                                 <label htmlFor="email" className="block">Email</label>
-                                <input className="form-control" type="email" id="email" name="email" />
+                                <input className="form-control" type="email" id="email" name="email" value={email} onChange={e => onChange(e)} required/>
                             </div>
                             <div className="form-item">
                                 <label htmlFor="password" className="block">Password</label>
-                                <input className="form-control" type="password" id="password" name="password" />
+                                <input className="form-control" type="password" id="password" name="password" value={password} onChange={e => onChange(e)} required
+                                minLength="6" />
                             </div>
                             <div className="form-item">
                                 <label htmlFor="passwordTwo" className="block">Confirm Password</label>
-                                <input className="form-control" type="password" id="passwordTwo" name="passwordTwo" />
+                                <input className="form-control" type="password" id="passwordTwo" name="passwordTwo" value={passwordTwo} onChange={e => onChange(e)} required minLength="6" />
                             </div>
                             <div className="form-item">
                                 <input className="form-control btn" type="submit" id="submit-btn" value="Submit" />
