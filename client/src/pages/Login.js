24345f397c1f17272da460de0ca1401a3a05f956
Regina Zaripova <rnloftz@gmail.com>
2020-12-01 23:59:15 +1000

    changed login component to func

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,42 +1,61 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import {Link} from 'react-router-dom'
 
-export class Login extends Component {
-    render() {
-        return (
-            <div className="container flex-center-row">
-            <div  className="flex-page greeting">
-                <h1 className="logo">Tasks Manager</h1>
-                <p>More tasks please!</p>
-            </div>
-            <div className="flex-page">
-                <div className="flex-center-column login-content">
-                    <div className="card flex-center-column">
-                        <div className="flex-center-column form-container-sign">
-                            <h3>Sign In</h3>
-                            <form>
-                                <div className="form-item">
-                                    <label htmlFor="email" className="block">Email</label>
-                                    <input className="form-control" type="email" id="email" name="email" />
-                                </div>
-                                <div className="form-item">
-                                    <label htmlFor="password" className="block">Password</label>
-                                    <input className="form-control" type="password" id="password" name="password" />
-                                </div>
-                                <div className="form-item">
-                                    <input className="form-control btn" type="submit" id="submit-btn" value="Submit" />
-                                </div>
-                            </form>
-                        </div>
-                        <p>Do not have an account? <Link to="/register">Register here!</Link> </p>
+
+export default function Login() {
+
+    const [formData, setFormData] = useState({
+        email:  "",
+        password: "",
+    });
+
+    const {email, password} = formData;
+
+   const onChange = e => setFormData({...formData, [e.target.name]: e.target.value});
+
+    const onSubmit = e => {
+        e.preventDefault();
+
+        if(password !== passwordTwo){
+            console.log("Passwords do not match")
+        } else {
+            console.log(formData)
+        }
+    }
+    
+
+    return (
+        <div className="container flex-center-row">
+        <div  className="flex-page greeting">
+            <h1 className="logo">Tasks Manager</h1>
+            <p>More tasks please!</p>
+        </div>
+        <div className="flex-page">
+            <div  className="flex-center-column login-content">
+                <div className="card flex-center-column">
+                    <div  className="flex-center-column form-container-sign">
+                        <h3>Sign Up</h3>
+                        <form onSubmit={e => onSubmit(e)}>
+                            <div className="form-item">
+                                <label htmlFor="email" className="block">Email</label>
+                                <input className="form-control" type="email" id="email" name="email" value={email} onChange={e => onChange(e)} required/>
+                            </div>
+                            <div className="form-item">
+                                <label htmlFor="password" className="block">Password</label>
+                                <input className="form-control" type="password" id="password" name="password" value={password} onChange={e => onChange(e)} required
+                                minLength="6" />
+                            </div>
+                            <div className="form-item">
+                                <input className="form-control btn" type="submit" id="submit-btn" value="Submit" />
+                            </div>
+                        </form>
                     </div>
+                    <p>Do not have an account? <Link to="/register">Sign Up here!</Link> </p>
                 </div>
-                
             </div>
             
         </div>
-        )
-    }
+        
+    </div>
+    )
 }
-
-export default Login
