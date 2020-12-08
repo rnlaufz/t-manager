73540bde09dbffad9dacd2579c86a5700f4d810e
Regina Zaripova <rnloftz@gmail.com>
2020-12-08 23:44:10 +1000

    added login auth action created login action types

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 import {setAlert} from "./alert";
 
-import {REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR} from './types';
+import {REGISTER_SUCCESS, REGISTER_FAIL, USER_LOADED, AUTH_ERROR, LOGIN_FAIL, LOGIN_SUCCESS} from './types';
 import setAuthToken from '../utils/setAuthToken'
 
 // Load user 
@@ -50,6 +50,36 @@ export const register = ({name, email, password}) => async dispatch => {
 
         dispatch({
             type: REGISTER_FAIL
+        });
+    }
+};
+
+// Login User
+export const login = (email, password) => async dispatch => {
+    const config = {
+        headers: {
+            'Content-Type': 'application/json '
+        }
+    }
+
+    const body = JSON.stringify({email, password});
+
+    try {
+        const res = await axios.post('/api/auth', body, config);
+        dispatch({
+            type: LOGIN_SUCCESS,
+            payload: res.data
+        });
+    } catch (err) {
+
+        const errors = err.response.data.errors;
+        
+        if(errors){
+            errors.forEach(error => dispatch(setAlert(error.msg, "danger")));
+        }
+
+        dispatch({
+            type: LOGIN_FAIL
         });
     }
 }
