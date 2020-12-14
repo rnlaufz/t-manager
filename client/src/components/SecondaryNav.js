4fe6f2844acf7411a10d2c077bbe9a130b935125
Regina Zaripova <rnloftz@gmail.com>
2020-12-14 20:00:24 +1000

    fixed authentication bug | nav links change if user authenticated/not

--- client/src/components/SecondaryNav.js
+++ client/src/components/SecondaryNav.js
@@ -8,21 +8,29 @@ import {logout} from '../actions/auth';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faCog, faPlus, faSignOutAlt} from '@fortawesome/free-solid-svg-icons'
 
+const SecondaryNav = ( {auth: {isAuthenticated, loading}, logout}) => {
 
-// @TO_DO: brainstorm about possible layout changes 
-
-const SecondaryNav = ({auth: {isAuthenticated, loading}, logout}) => {
-  return (
-    <div className="flex-center-row secondary-nav">
-    <h2><span className="logo title">Tasks Manager</span> | {this.props.navTitle}</h2>
+  const authLinks = (
     <ul className="flex-center-row">
-      
-        <li><Link to='/new_task'><i><FontAwesomeIcon icon={faPlus} /></i></Link></li>
+       <li><Link to='/new_task'><i><FontAwesomeIcon icon={faPlus} /></i></Link></li>
         <li><Link to="/settings/user_data"><i ><FontAwesomeIcon icon={faCog} /></i></Link></li>
         {/* This one temporarily leads to index page */}
-        <li><Link to="/"><i><FontAwesomeIcon icon={faSignOutAlt} /></i></Link></li>
-       
+        <li><Link onClick={logout}><i><FontAwesomeIcon icon={faSignOutAlt} /></i></Link></li>
     </ul>
+  )
+
+  const registerLinks = (
+    <ul className="flex-center-row">
+    <li><Link to='/register'>Sing Up</Link></li>
+     <li><Link to="/login">Sign In</Link></li>
+    
+ </ul>
+  )
+
+  return (
+    <div className="flex-center-row secondary-nav">
+    <h2><span className="logo title">Tasks Manager</span></h2>
+   {!loading && (<React.Fragment>{isAuthenticated ? authLinks : registerLinks}</React.Fragment>)}
 </div> 
   )
 }
