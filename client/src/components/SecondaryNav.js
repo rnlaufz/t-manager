f18281b80571366192fd67969731af5f6356aeab
Regina Zaripova <rnloftz@gmail.com>
2020-12-15 15:37:16 +1000

    changed back secNav from funt to class | fixed props import issue in secNav

--- client/src/components/SecondaryNav.js
+++ client/src/components/SecondaryNav.js
@@ -1,40 +1,53 @@
-import React from 'react';
+import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 import {connect} from 'react-redux';
 import propTypes from 'prop-types';
 import {logout} from '../actions/auth';
+import auth from '../reducers/auth'
 
 // Icons
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {faCog, faPlus, faSignOutAlt} from '@fortawesome/free-solid-svg-icons'
+import {faCog, faPlus, faSignOutAlt} from '@fortawesome/free-solid-svg-icons';
 
-const SecondaryNav = ( {auth: {isAuthenticated, loading}, logout}) => {
 
-  const authLinks = (
-    <ul className="flex-center-row">
-       <li><Link to='/new_task'><i><FontAwesomeIcon icon={faPlus} /></i></Link></li>
-        <li><Link to="/settings/user_data"><i ><FontAwesomeIcon icon={faCog} /></i></Link></li>
-        {/* This one temporarily leads to index page */}
-        <li><Link onClick={logout}><i><FontAwesomeIcon icon={faSignOutAlt} /></i></Link></li>
-    </ul>
-  )
+// @TO_DO change component to class based
 
-  const registerLinks = (
-    <ul className="flex-center-row">
-    <li><Link to='/register'>Sing Up</Link></li>
-     <li><Link to="/login">Sign In</Link></li>
+export class SecondaryNav extends Component {
+
+  constructor(){
+    super()
     
- </ul>
-  )
+  }
 
-  return (
-    <div className="flex-center-row secondary-nav">
-    <h2><span className="logo title">Tasks Manager</span></h2>
+  render() {
+    const {isAuthenticated, loading} = auth;
+    const {logout} = auth;
+    const authLinks = (
+      <ul className="flex-center-row">
+         <li><Link to='/new_task'><i><FontAwesomeIcon icon={faPlus} /></i></Link></li>
+          <li><Link to="/settings/user_data"><i ><FontAwesomeIcon icon={faCog} /></i></Link></li>
+          {/* This one temporarily leads to index page */}
+          <li><Link onClick={logout}><i><FontAwesomeIcon icon={faSignOutAlt} /></i></Link></li>
+      </ul>
+    )
+  
+  const registerLinks = (
+      <ul className="flex-center-row">
+      <li><Link to='/register'>Sing Up</Link></li>
+       <li><Link to="/login">Sign In</Link></li>
+      
+   </ul>
+    )
+    // const {auth: {isAuthenticated, loading}, logout}
+    return (
+      <div className="flex-center-row secondary-nav">
+      {/* Fix func */}
+    <h2><span className="logo title">Tasks Manager</span> | {this.props.navTitle}</h2>
    {!loading && (<React.Fragment>{isAuthenticated ? authLinks : registerLinks}</React.Fragment>)}
 </div> 
-  )
+    )
+  }
 }
-
 
 SecondaryNav.propTypes = {
   logout: propTypes.func.isRequired,
